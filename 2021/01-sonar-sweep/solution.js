9f0e0f1876c98ff0ce0402e4609841389ee89e2d
Joey Emerson <joey@emerson.dev>
2021-12-01 10:36:29 -0600

    finished day 1

---
+++ 2021/01-sonar-sweep/solution.js
@@ -0,0 +1,29 @@
+fs = require('fs');
+
+const input = fs
+  .readFileSync(__dirname + '/input')
+  .toString()
+  .split('\n')
+  .map(Number);
+
+input.pop(); // remove blank line at end of input file
+
+const countIncreases = arr => {
+  return arr.reduce((acc, cur, idx) => {
+    return acc + (cur > arr[idx - 1] ? 1 : 0);
+  }, 0);
+};
+
+// Part 1: How many measurements are larger than the previous measurement?
+const p1 = arr => {
+  return countIncreases(arr);
+};
+
+// Part 2: Consider sums of a three-measurement sliding window. How many sums are larger than the previous sum?
+const p2 = arr => {
+  const threeSums = arr.slice(2).map((el, idx) => el + arr[idx] + arr[idx + 1]);
+  return countIncreases(threeSums);
+};
+
+console.log(p1(input));
+console.log(p2(input));
