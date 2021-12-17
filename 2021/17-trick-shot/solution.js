35fe2cdb165c2403729b852b19c248183538bee1
Joey Emerson <joey@emerson.dev>
2021-12-17 13:24:51 -0600

    d17 added comments

--- 2021/17-trick-shot/solution.js
+++ 2021/17-trick-shot/solution.js
@@ -15,20 +15,22 @@ const maxHeight = initialY => (initialY * (initialY + 1)) / 2;
 // Part 1: Find the initial velocity that causes the probe to reach the highest y position and still eventually
 // be within the target area after any step. What is the highest y position it reaches on this trajectory?
 const p1 = (loX, hiX, loY, hiY) => {
-  const maxAbxY = Math.max(Math.abs(loY), Math.abs(hiY));
+  const maxAbsY = Math.max(Math.abs(loY), Math.abs(hiY));
 
   for (let x = 1; x <= hiX; ++x) {
-    for (let y = maxAbxY; y >= -maxAbxY; --y) {
+    for (let y = maxAbsY; y >= -maxAbsY; --y) {
       let curX = 0;
       let curY = 0;
       let velX = x;
       let velY = y;
 
       while (!pastTargetArea(curY, loY)) {
+        // Since we're starting with the lowest x value and the highest y value,
+        // the first correct velocity we find will be the maximum y.
         if (inTargetArea(curX, curY, loX, hiX, loY, hiY)) return maxHeight(y);
         curX += velX;
         curY += velY;
-        velX -= velX === 0 ? 0 : 1;
+        velX -= velX !== 0;
         velY -= 1;
       }
 
@@ -39,8 +41,12 @@ const p1 = (loX, hiX, loY, hiY) => {
 
 // Part 2: How many distinct initial velocity values cause the probe to be within the target area after any step?
 const p2 = (loX, hiX, loY, hiY) => {
+  // Based on the example input and results, it seemed that possible y values would lie
+  // between the largest absolute value of each targetY and that negated value.
+  // This worked for my input, but YMMV. Planning to revisit and do some proper maths here...
   const maxAbsY = Math.max(Math.abs(loY), Math.abs(hiY));
-  const distinctVelocities = new Set();
+
+  let distinctVelocities = 0;
 
   for (let x = 1; x <= hiX; ++x) {
     for (let y = maxAbsY; y >= -maxAbsY; --y) {
@@ -50,10 +56,14 @@ const p2 = (loX, hiX, loY, hiY) => {
       let velY = y;
 
       while (!pastTargetArea(curY, loY)) {
-        if (inTargetArea(curX, curY, loX, hiX, loY, hiY)) distinctVelocities.add(x + ' ' + y);
+        if (inTargetArea(curX, curY, loX, hiX, loY, hiY)) {
+          ++distinctVelocities;
+          break;
+        }
+
         curX += velX;
         curY += velY;
-        velX -= velX === 0 ? 0 : 1;
+        velX -= velX !== 0;
         velY -= 1;
       }
 
@@ -61,7 +71,7 @@ const p2 = (loX, hiX, loY, hiY) => {
     }
   }
 
-  return distinctVelocities.size;
+  return distinctVelocities;
 };
 
 console.time('Part 1 Time');
