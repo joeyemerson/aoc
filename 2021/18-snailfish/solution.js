0373cd5c173bf038faac0eba9df3066903dc3d13
Joey Emerson <joey@emerson.dev>
2021-12-18 13:40:19 -0600

    finished day 18

---
+++ 2021/18-snailfish/solution.js
@@ -0,0 +1,181 @@
+const fs = require('fs');
+
+const input = fs
+  .readFileSync('./input')
+  .toString()
+  .split('\n')
+  .filter(line => line !== '');
+
+const TreeNode = function (val) {
+  this.val = val;
+  this.left = null;
+  this.right = null;
+  this.parent = null;
+};
+
+const constructTree = (str, parent) => {
+  const root = new TreeNode(-1);
+  root.parent = parent;
+
+  let open = 0;
+  let leftStr = '';
+  let rightStr = '';
+
+  for (let i = 0; i < str.length; ++i) {
+    if (str[i] === '[') {
+      ++open;
+      if (open > 1) leftStr += str[i];
+    } else if (str[i] === ']') {
+      --open;
+      leftStr += str[i];
+    } else if (str[i] === ',' && open === 1) {
+      rightStr = str.slice(i + 1, -1);
+      break;
+    } else leftStr += str[i];
+  }
+
+  if (leftStr.length > 1) {
+    root.left = constructTree(leftStr, root);
+  } else {
+    root.left = new TreeNode(parseInt(leftStr));
+    root.left.parent = root;
+  }
+
+  if (rightStr.length > 1) {
+    root.right = constructTree(rightStr, root);
+  } else {
+    root.right = new TreeNode(parseInt(rightStr));
+    root.right.parent = root;
+  }
+
+  return root;
+};
+
+const mergeTrees = (a, b) => {
+  const newRoot = new TreeNode(-1);
+  newRoot.left = a;
+  newRoot.right = b;
+  a.parent = newRoot;
+  b.parent = newRoot;
+  return newRoot;
+};
+
+const getExplodeNode = (node, depth = 0) => {
+  if (!node) return null;
+  if (depth >= 4 && node.val === -1 && node.left.val !== -1 && node.right.val !== -1) return node;
+  return getExplodeNode(node.left, depth + 1) || getExplodeNode(node.right, depth + 1);
+};
+
+const explode = node => {
+  const _explode = (node, prevNode, addVal, dir, traversingUp) => {
+    if (!node[dir]) return;
+
+    if (node[dir] !== prevNode) {
+      if (traversingUp && node[dir].val === -1)
+        _explode(node[dir], null, addVal, dir === 'left' ? 'right' : 'left', false);
+      else if (node[dir].val === -1) _explode(node[dir], null, addVal, dir, traversingUp);
+      else node[dir].val += addVal;
+    } else if (node.parent) {
+      _explode(node.parent, node, addVal, dir, traversingUp);
+    }
+  };
+
+  const leftVal = node.left.val;
+  const rightVal = node.right.val;
+  node.val = 0;
+  node.left = null;
+  node.right = null;
+  _explode(node.parent, node, leftVal, 'left', true);
+  _explode(node.parent, node, rightVal, 'right', true);
+};
+
+const getSplitNode = node => {
+  if (!node) return null;
+  if (node.val > 9) return node;
+  return getSplitNode(node.left) || getSplitNode(node.right);
+};
+
+const split = node => {
+  const val = node.val;
+  node.val = -1;
+  node.left = new TreeNode(Math.floor(val / 2));
+  node.right = new TreeNode(Math.ceil(val / 2));
+  node.left.parent = node;
+  node.right.parent = node;
+};
+
+const magnitude = node => {
+  if (!node) return 0;
+  if (node.val !== -1) return node.val;
+  return 3 * magnitude(node.left) + 2 * magnitude(node.right);
+};
+
+// Part 1: Add up all of the snailfish numbers from the homework assignment in the order they appear.
+// What is the magnitude of the final sum?
+const p1 = input => {
+  let root = constructTree(input.shift());
+
+  for (const line of input) {
+    root = mergeTrees(root, constructTree(line, null));
+    let madeChange;
+
+    do {
+      madeChange = false;
+      const explodeNode = getExplodeNode(root);
+
+      if (explodeNode) {
+        explode(explodeNode);
+        madeChange = true;
+      } else {
+        const splitNode = getSplitNode(root);
+        if (splitNode) {
+          split(splitNode);
+          madeChange = true;
+        }
+      }
+    } while (madeChange);
+  }
+
+  return magnitude(root);
+};
+
+// Part 2: What is the largest magnitude of any sum of two different snailfish numbers from the homework assignment?
+const p2 = input => {
+  let maxMagnitude = 0;
+
+  for (let i = 0; i < input.length; ++i) {
+    for (let j = i + 1; j < input.length; ++j) {
+      const root = mergeTrees(constructTree(input[i], null), constructTree(input[j], null));
+      let madeChange;
+
+      do {
+        madeChange = false;
+        const explodeNode = getExplodeNode(root);
+
+        if (explodeNode) {
+          explode(explodeNode);
+          madeChange = true;
+        } else {
+          const splitNode = getSplitNode(root);
+          if (splitNode) {
+            split(splitNode);
+            madeChange = true;
+          }
+        }
+      } while (madeChange);
+
+      maxMagnitude = Math.max(maxMagnitude, magnitude(root));
+    }
+  }
+
+  return maxMagnitude;
+};
+
+console.time('Part 1 Time');
+console.log('Part 1:', p1(input));
+console.timeEnd('Part 1 Time');
+console.log();
+
+console.time('Part 2 Time');
+console.log('Part 2:', p2(input));
+console.timeEnd('Part 2 Time');
