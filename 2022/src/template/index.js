68533652b253ffea5301b1de0946dcb4e3a678ee
Joey Emerson <joey@emerson.dev>
2022-11-28 16:13:52 -0600

    added aoc runner to project for 2022 solutions

---
+++ 2022/src/template/index.js
@@ -0,0 +1,38 @@
+import run from "aocrunner";
+
+const parseInput = (rawInput) => rawInput;
+
+const part1 = (rawInput) => {
+  const input = parseInput(rawInput);
+
+  return;
+};
+
+const part2 = (rawInput) => {
+  const input = parseInput(rawInput);
+
+  return;
+};
+
+run({
+  part1: {
+    tests: [
+      // {
+      //   input: ``,
+      //   expected: "",
+      // },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      // {
+      //   input: ``,
+      //   expected: "",
+      // },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+});
