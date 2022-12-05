fc4a8a2a492583efd71031cdb576133b669165da
Joey Emerson <joey@emerson.dev>
2022-12-05 09:47:07 -0600

    updated template for aocrunner

--- 2022/src/template/index.js
+++ 2022/src/template/index.js
@@ -1,38 +1,38 @@
-import run from "aocrunner";
+import run from 'aocrunner';
 
 const parseInput = (rawInput) => rawInput;
 
 const part1 = (rawInput) => {
-  const input = parseInput(rawInput);
+    const input = parseInput(rawInput);
 
-  return;
+    return;
 };
 
 const part2 = (rawInput) => {
-  const input = parseInput(rawInput);
+    const input = parseInput(rawInput);
 
-  return;
+    return;
 };
 
 run({
-  part1: {
-    tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
-    ],
-    solution: part1,
-  },
-  part2: {
-    tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
-    ],
-    solution: part2,
-  },
-  trimTestInputs: true,
-  onlyTests: false,
+    part1: {
+        tests: [
+            // {
+            //   input: ``,
+            //   expected: "",
+            // },
+        ],
+        solution: part1,
+    },
+    part2: {
+        tests: [
+            // {
+            //   input: ``,
+            //   expected: "",
+            // },
+        ],
+        solution: part2,
+    },
+    trimTestInputs: false,
+    onlyTests: false,
 });
